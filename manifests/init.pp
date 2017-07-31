b38bfb9925a18f59dba7a0842233612ab0536a5f
Andreas Ziethen <az@ziethen.net>
2017-07-31 14:49:32 +0200

    add require of apt::update

--- manifests/init.pp
+++ manifests/init.pp
@@ -39,9 +39,11 @@ class sc_java(
     exec { 'acceptLicense':
       command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections',
       unless => "/usr/bin/debconf-show oracle-java$java_version-installer | grep 'accepted-oracle-license-v1-1: true'",
+      require => Class['apt::update'],
     }->
 
     package { "oracle-java$java_version-installer":
       ensure => 'installed',
+      require => Class['apt::update'],
     }
 }
